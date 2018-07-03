84213ee04f7cb8d77805cd368f35f362623e0e09
Ana Cantu <anacantu093@gmail.com>
2018-07-03 18:44:14 -0500

    create Django project

---
+++ mysite/mysite/settings/development.py
@@ -0,0 +1,17 @@
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
+
+from .base import *
+
+config.read('/usr/local/etc/settings.ini')
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = config.get('keys','SECRET_KEY')
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
---
+++ mysite/mysite/settings/production.py
@@ -0,0 +1,12 @@
+from .base import *
+
+config.read('/etc/kaizen/settings.ini')
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = config.get('keys','SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ['18.222.154.241', 'ec2-18-222-154-241.us-east-2.compute.amazonaws.com']
