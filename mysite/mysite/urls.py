5966282d1b564b460b3ea2e4f27e22fe94e70b9e
Ana Cantu <anacantu093@gmail.com>
2018-07-04 13:41:18 -0500

    Add path to proposal app URLS

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
+    path('proposal/', include('proposal.urls')),
 ]
