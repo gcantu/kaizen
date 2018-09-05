8e273cd3f0a0b70ef1de8d79b40ca562bdc7bc39
Ana Cantu <anacantu093@gmail.com>
2018-09-05 17:20:56 -0500

    Display a list of pending proposals

--- mysite/dashboard/views.py
+++ mysite/dashboard/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from proposal.models import proposal
 
 
 def Home(request):
-    return render(request, 'dashboard/home.html')
+    p = proposal.objects.filter(pending=1)
+
+    return render(request, 'dashboard/home.html', {'proposals': p})
