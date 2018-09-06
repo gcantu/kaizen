4201c5ba5fe52f4c03f687743223ea20aace0133
Ana Cantu <anacantu093@gmail.com>
2018-09-05 19:23:57 -0500

    fix bug... 0=true

--- mysite/dashboard/views.py
+++ mysite/dashboard/views.py
@@ -3,6 +3,6 @@ from proposal.models import proposal
 
 
 def Home(request):
-    p = proposal.objects.filter(pending=1)
+    p = proposal.objects.filter(pending=0)
 
     return render(request, 'dashboard/home.html', {'proposals': p})
