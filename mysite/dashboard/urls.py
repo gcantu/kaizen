671520bafe9a1f84115626dd34c21c2f3358e41d
Ana Cantu <anacantu093@gmail.com>
2018-09-05 19:54:43 -0500

    add manufacturing report list

--- mysite/dashboard/urls.py
+++ mysite/dashboard/urls.py
@@ -5,4 +5,5 @@ app_name = 'dashboard'
 
 urlpatterns = [
     path('home', views.Home, name='home'),
+    path('manufacturing', views.mReport, name='manufacturing-list'),
 ]
--- mysite/dashboard/views.py
+++ mysite/dashboard/views.py
@@ -5,4 +5,10 @@ from proposal.models import proposal
 def Home(request):
     p = proposal.objects.filter(pending=0)
 
-    return render(request, 'dashboard/home.html', {'proposals': p})
+    return render(request, 'dashboard/home.html', {'proposals': p, 'pending': True})
+
+
+def mReport(request):
+    p = proposal.objects.filter(pending=1)
+
+    return render(request, 'dashboard/home.html', {'proposals': p, 'pending': False})
