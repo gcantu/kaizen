e959280c107dfc698a2bf7f579c4a63464f3be55
Ana Cantu <anacantu093@gmail.com>
2018-09-10 12:50:46 -0500

    Add "proposal" field to form
    - Field is needed for editing line items

--- mysite/proposal/forms.py
+++ mysite/proposal/forms.py
@@ -27,6 +27,6 @@ from django.forms.models import inlineformset_factory
 proposalLineItemFormSet = inlineformset_factory(
     proposal,
     line_item,
-    fields=('product', 'shutter_type', 'finish', 'stain', 'color', 'location', 'mount', 'trim', 'trim_style', 'louver', 'hinges', 'hinge_color', 'panels', 't_post', 'tilt_rod', 'separate_parts', 'width', 'width_fraction', 'height', 'height_fraction', 'height_left', 'height_left_fraction', 'height_right', 'height_right_fraction', 'height_center',    'height_center_fraction', 'quantity', 'price', 'approved',),
+    fields=('product', 'shutter_type', 'finish', 'stain', 'color', 'location', 'mount', 'trim', 'trim_style', 'louver', 'hinges', 'hinge_color', 'panels', 't_post', 'tilt_rod', 'separate_parts', 'width', 'width_fraction', 'height', 'height_fraction', 'height_left', 'height_left_fraction', 'height_right', 'height_right_fraction', 'height_center', 'height_center_fraction', 'quantity', 'price', 'approved', 'proposal',),
     extra=0
 )
